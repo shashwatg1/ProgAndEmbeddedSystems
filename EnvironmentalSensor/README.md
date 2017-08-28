40df58fcf3b07ed36cd6a080d5af6c122e1aac89
Shashwat Gupta <shashwatg1@gmail.com>
2017-08-28 17:21:53 +0530

    New

--- EnvironmentalSensor/README.md
+++ EnvironmentalSensor/README.md
@@ -6,9 +6,13 @@ This app is created for Programmable and Embedded Systems.
 
 ## Screenshots:
 
+<img src="Screenshot/Screenshot_0.png" width="40%">
+
 <img src="Screenshot/Screenshot_1.png" width="40%">
 
 <img src="Screenshot/Screenshot_2.png" width="40%">
+
+<img src="Screenshot/Screenshot_3.png" width="40%">
 
 ## Author
 
--- EnvironmentalSensor/app/src/main/java/com/example/shashwatgupta/environmentalsensor/MainActivity.java
+++ EnvironmentalSensor/app/src/main/java/com/example/shashwatgupta/environmentalsensor/MainActivity.java
@@ -1,149 +1,173 @@
 package com.example.shashwatgupta.environmentalsensor;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Context;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
-import android.os.Environment;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.util.Log;
 import android.widget.TextView;
-import android.widget.Toast;
 
-
-import java.util.Locale;
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.LegendRenderer;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
-    private int on = 0;
-    Button switch1;
+    private SensorManager mSensorManager;
+    private Sensor mSensor;
+    private TextView LI, Accel, Gyro, Magno;
+
+    private final String TAG = "GraphSensors";
+    private GraphView mGraphGyro;
+    private GraphView mGraphAccel;
+    private GraphView mGraphLI;
+    private GraphView mGraphMag;
+    private LineGraphSeries<DataPoint> mSeriesGyro;
+    private LineGraphSeries<DataPoint> mSeriesAccel;
+    private LineGraphSeries<DataPoint> mSeriesMag;
+    private LineGraphSeries<DataPoint> mSeriesLI;
+    private double graphLastGyroXValue = 5d;
+    private double graphLastAccelXValue = 5d;
+    private double graphLastLIXValue = 5d;
+    private double graphLastMagXValue = 5d;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        switch1 = (Button) findViewById(R.id.switch1);
+        LI = (TextView)findViewById(R.id.light);
+        Accel = (TextView)findViewById(R.id.accel);
+        Gyro = (TextView)findViewById(R.id.gyro);
+        Magno = (TextView)findViewById(R.id.magne);
+        startLI();
+        startAccel();
+        startGyro();
+        startMag();
+        mGraphGyro = initGraph(R.id.graphGyro, "Sensor.TYPE_GYROSCOPE");
+        mGraphAccel = initGraph(R.id.graphAccel, "Sensor.TYPE_ACCELEROMETER");
+        mGraphLI = initGraph(R.id.graphLI, "Sensor.TYPE_LIGHT");
+        mGraphMag = initGraph(R.id.graphMag, "Sensor.TYPE_MAGNETIC_FIELD");
 
-        switch1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                launchActivity();
-            }
-        });
+        mSeriesGyro = initSeries(Color.DKGRAY, "Gyro");
+        mGraphGyro.addSeries(mSeriesGyro);
+        startGyro();
+
+        // ACCEL
+        mSeriesAccel = initSeries(Color.CYAN, "Accel");
+        mGraphAccel.addSeries(mSeriesAccel);
+        startAccel();
+
+        // LI
+        mSeriesLI = initSeries(Color.RED, "Light Int");
+        mGraphLI.addSeries(mSeriesLI);
+        startLI();
+
+        // Magnatic
+        mSeriesMag = initSeries(Color.BLUE, "Magne");
+        mGraphMag.addSeries(mSeriesMag);
+        startMag();
     }
 
-    private void launchActivity() {
-        Intent intent = new Intent(this, graphActivity.class);
-        startActivity(intent);
+    public GraphView initGraph(int id, String title) {
+        GraphView graph = (GraphView) findViewById(id);
+        graph.getViewport().setXAxisBoundsManual(true);
+        graph.getViewport().setMinX(0);
+        graph.getViewport().setMaxX(5);
+        graph.getGridLabelRenderer().setLabelVerticalWidth(100);
+        graph.setTitle(title);
+        graph.getGridLabelRenderer().setHorizontalLabelsVisible(false);
+        graph.getLegendRenderer().setVisible(true);
+        graph.getLegendRenderer().setAlign(LegendRenderer.LegendAlign.TOP);
+        return graph;
+    }
+
+    public LineGraphSeries<DataPoint> initSeries(int color, String title) {
+        LineGraphSeries<DataPoint> series;
+        series = new LineGraphSeries<>();
+        series.setDrawDataPoints(true);
+        series.setDrawBackground(false);
+        series.setColor(color);
+        series.setTitle(title);
+        return series;
+    }
+
+    public void startLI(){
+        mSensorManager = (SensorManager) this.getSystemService(Context.SENSOR_SERVICE);
+        mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+        mSensorManager.registerListener(this, mSensor, SensorManager.SENSOR_DELAY_NORMAL);}
+
+    public void startGyro(){
+        mSensorManager = (SensorManager) this.getSystemService(Context.SENSOR_SERVICE);
+        mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+        mSensorManager.registerListener(this, mSensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    public void startAccel(){
+        mSensorManager = (SensorManager) this.getSystemService(Context.SENSOR_SERVICE);
+        mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mSensorManager.registerListener(this, mSensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    public void startMag(){
+        mSensorManager = (SensorManager) this.getSystemService(Context.SENSOR_SERVICE);
+        mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+        mSensorManager.registerListener(this, mSensor, SensorManager.SENSOR_DELAY_NORMAL);
     }
 
     @Override
-    protected void onResume() {
-        super.onResume();
-        if(on==1) return;
-        SensorManager sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-        if(sensorManager != null && sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) != null) {
-            Sensor sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-            sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_UI);
-        }
-        else {
-            Toast.makeText(getApplicationContext(), "Accelerometer Error", Toast.LENGTH_LONG).show();
-            System.exit(1);
-        }
-        if(sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE) != null) {
-            Sensor sensor = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
-            sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_UI);
-        }
-        else {
-            Toast.makeText(getApplicationContext(), "Gyroscope Error", Toast.LENGTH_LONG).show();
-            System.exit(1);
-        }
-        if(sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD) != null) {
-            Sensor sensor = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
-            sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_UI);
-        }
-        else {
-            Toast.makeText(getApplicationContext(), "Magnetometer Error", Toast.LENGTH_LONG).show();
-            System.exit(1);
-        }
-    }
+    public void onSensorChanged(SensorEvent event) {
+        //event.values[x,y,z]
+        if(event.sensor.getType() == Sensor.TYPE_GYROSCOPE) {
+            graphLastGyroXValue += 0.15d;
+            double gx,gy,gz, gyro;
+            gx = event.values[0];
+            gy = event.values[1];
+            gz = event.values[2];
+            gyro = Math.sqrt(gx * gx + gy * gy + gz * gz);
+            Gyro.setText("Gyroscope: " + gyro);
+            mSeriesGyro.appendData(new DataPoint(graphLastGyroXValue, gyro), true, 33);
 
-    @Override
-    protected void onPause() {
-        if(on==1) {
-            super.onPause();
-            return;
+        } else if(event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            graphLastAccelXValue += 0.15d;
+            double ax,ay,az, accel;
+            ax = event.values[0];
+            ay = event.values[1];
+            az = event.values[2];
+            accel = Math.sqrt(ax * ax + ay * ay + az * az);
+            Accel.setText("Acceleration: " + accel);
+            mSeriesAccel.appendData(new DataPoint(graphLastAccelXValue, accel), true, 33);
         }
-        SensorManager sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-        if(sensorManager != null && sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) != null) {
-            Sensor sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-            sensorManager.unregisterListener(this, sensor);
-        }
-        else {
-            Toast.makeText(getApplicationContext(), "Accelerometer Error", Toast.LENGTH_LONG).show();
-            System.exit(1);
-        }
-        if(sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE) != null) {
-            Sensor sensor = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
-            sensorManager.unregisterListener(this, sensor);
-        }
-        else {
-            Toast.makeText(getApplicationContext(), "Gyroscope Error", Toast.LENGTH_LONG).show();
-            System.exit(1);
-        }
-        if(sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD) != null) {
-            Sensor sensor = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
-            sensorManager.unregisterListener(this, sensor);
-        }
-        else {
-            Toast.makeText(getApplicationContext(), "Magnetometer Error", Toast.LENGTH_LONG).show();
-            System.exit(1);
-        }
-        super.onPause();
-    }
+        else if(event.sensor.getType() == Sensor.TYPE_LIGHT) {
+            LI.setText("Temperature: " + event.values[0]);
+            graphLastLIXValue += 0.15d;
+            mSeriesLI.appendData(new DataPoint(graphLastLIXValue, event.values[0]), true, 33);
 
-    @Override
-    public void onSensorChanged(SensorEvent sensorEvent) {
-        if(sensorEvent==null) return;
-        if(sensorEvent.sensor.getType()==Sensor.TYPE_ACCELEROMETER) {
-            float x = sensorEvent.values[0];
-            float y = sensorEvent.values[1];
-            float z = sensorEvent.values[2];
-            TextView xtv = (TextView) findViewById(R.id.ax);
-            TextView ytv = (TextView) findViewById(R.id.ay);
-            TextView ztv = (TextView) findViewById(R.id.az);
-            xtv.setText(String.format(Locale.getDefault(), "%.9f", x));
-            ytv.setText(String.format(Locale.getDefault(), "%.9f", y));
-            ztv.setText(String.format(Locale.getDefault(), "%.9f", z));
+        } else if(event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) {
+            graphLastMagXValue += 0.15d;
+            double mx,my,mz, magne;
+            mx = event.values[0];
+            my = event.values[1];
+            mz = event.values[2];
+
+            magne = Math.sqrt(mx * mx + my * my + mz * mz);
+            Magno.setText("Mangntometer: " + magne);
+            mSeriesMag.appendData(new DataPoint(graphLastMagXValue,magne ), true, 33);
         }
-        if(sensorEvent.sensor.getType()== Sensor.TYPE_GYROSCOPE) {
-            float x = sensorEvent.values[0];
-            float y = sensorEvent.values[1];
-            float z = sensorEvent.values[2];
-            TextView xtv = (TextView) findViewById(R.id.gx);
-            TextView ytv = (TextView) findViewById(R.id.gy);
-            TextView ztv = (TextView) findViewById(R.id.gz);
-            xtv.setText(String.format(Locale.getDefault(), "%.9f", x));
-            ytv.setText(String.format(Locale.getDefault(), "%.9f", y));
-            ztv.setText(String.format(Locale.getDefault(), "%.9f", z));
-        }
-        if(sensorEvent.sensor.getType()==Sensor.TYPE_MAGNETIC_FIELD) {
-            float x = sensorEvent.values[0];
-            float y = sensorEvent.values[1];
-            float z = sensorEvent.values[2];
-            TextView xtv = (TextView) findViewById(R.id.mx);
-            TextView ytv = (TextView) findViewById(R.id.my);
-            TextView ztv = (TextView) findViewById(R.id.mz);
-            xtv.setText(String.format(Locale.getDefault(), "%.9f", x));
-            ytv.setText(String.format(Locale.getDefault(), "%.9f", y));
-            ztv.setText(String.format(Locale.getDefault(), "%.9f", z));
-        }
+        String dataString = String.valueOf(event.accuracy) + "," + String.valueOf(event.timestamp) + "," + String.valueOf(event.sensor.getType()) + "\n";
+        Log.d(TAG, "Data received: " + dataString);
     }
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int i) {
+        Log.d(TAG, "onAccuracyChanged called for the gyro");
     }
+
 }
